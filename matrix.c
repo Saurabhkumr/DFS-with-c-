316b0b48cb2c460db30b78d803f4bb4f7002c9e7
Saurabh <saurabhkr0927@gmail.com>
2024-02-18 18:01:53 +0530

    identity matrix added

--- matrix.c
+++ matrix.c
@@ -69,7 +69,7 @@ void subtraction(int arr1[][10], int arr2[][10], int row, int col)
         printf("\n");
     }
 }
-void inverse(int arr1[][10], int row, int col)
+void transpose(int arr1[][10], int row, int col)
 {
     int ans[10][10];
     int i, j;
@@ -80,7 +80,7 @@ void inverse(int arr1[][10], int row, int col)
             ans[i][j] = arr1[j][i];
         }
     }
-    printf("Resultant matrix : \n");
+    printf("Transpose matrix : \n");
     for (i = 0; i < row; i++)
     {
         for (j = 0; j < col; j++)
@@ -89,6 +89,62 @@ void inverse(int arr1[][10], int row, int col)
         }
         printf("\n");
     }
+}
+
+void diagonalSum(int arr1[][10], int arr2[][10], int row, int col)
+{
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i == j)
+            {
+                sum1 += arr1[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i == j)
+            {
+                sum2 += arr2[i][j];
+            }
+        }
+    }
+    printf("\nSum of diagonal of 1st matrix : %d", sum1);
+    printf("\nSum of diagonal of 2nd matrix : %d", sum2);
+}
+
+void indentity(int arr[][10], int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i != j)
+            {
+                if (arr[i][j] != 0)
+                {
+                    printf("\n Non-identity matrix");
+                    return;
+                }
+            }
+            else
+            {
+                if (arr[i][j] != 1)
+                {
+                    printf("\n Non-identity matrix");
+                    return;
+                }
+            }
+        }
+    }
+
+    printf("\nIdentity Matrix");
 }
 
 int main()
@@ -154,7 +210,9 @@ int main()
         printf("\n 1. Multiplication");
         printf("\n 2. Addition");
         printf("\n 3. Subtraction");
-        printf("\n 4. Inverse");
+        printf("\n 4. Transpose");
+        printf("\n 5. Sum of Diagonal");
+        printf("\n 6. Check for Identity");
         printf("\n Choose the operation you want to perform : ");
         scanf("%d", &opt);
 
@@ -187,20 +245,28 @@ int main()
 
         case 4:
             // Inverse
-            inverse(arr1, row1, col1);
+            transpose(arr1, row1, col1);
+            break;
+
+        case 5:
+            // Sum of Diagonals
+            diagonalSum(arr1, arr2, row1, col1);
+            break;
+
+        case 6:
+            // check for identity
+            indentity(arr1, row1, col1);
             break;
 
         default:
             printf("Inalid Input !");
-
         }
-        printf("Do you want to continue (y/n) : ");
+        printf("\nDo you want to continue (y/n) : ");
         scanf(" %c", &continueChoice);
 
-    } while(continueChoice == 'y');
-    
+    } while (continueChoice == 'y');
 
-        printf("Program Exit...");
+    printf("Program Exit...");
 
-        return 0;
+    return 0;
 }
